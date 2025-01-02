bb9f13151c5fbb10b03f08b5ed23ca42870daad5
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2025-01-02 14:45:54 +0000

    Add astro-pdf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import pdf from 'astro-pdf';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    pdf({
+      launch: { dumpio: true },
+      pages: { "/index.html": true },
+    }),
+  ]
+});
