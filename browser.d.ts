a4559efee40c42c2d88af243ed539231c846baa0
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2025-01-17 10:43:47 +0000

    Try to understand what config Paged exposes

--- browser.d.ts
+++ browser.d.ts
@@ -1,11 +1,20 @@
+interface PagedConfiguration {
+  after?: (done?: unknown /** Pagedjs Chunker instance */) => void | Promise<void>;
+  auto?: boolean;
+  before?: () => void | Promise<void>;
+  content?: DocumentFragment;
+  stylesheets?: (string | Record<string, string>)[];
+  renderTo?: Node;
+  settings?: {
+    hyphenGlyph?: string;
+    maxChars?: number;
+  };
+}
+
 /**
  * Enrich browser window with PagedJS-related names
  */
 interface Window {
   __pagedjs_render_complete__?: boolean;
-  PagedConfig?: {
-    after?: () => void | Promise<void>;
-    auto?: boolean;
-    before?: () => void | Promise<void>;
-  };
+  PagedConfig?: Partial<PagedConfiguration>;
 }
