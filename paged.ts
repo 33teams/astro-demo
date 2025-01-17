0d4256bd1cccc4d4ec99d24bbbb996d9cfd27f89
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2025-01-17 16:02:06 +0000

    Log as much information as possible from client-side JS

--- paged.ts
+++ paged.ts
@@ -9,19 +9,17 @@ import type { Page } from "puppeteer";
 const pagedPolyfill = join(dirname(fileURLToPath(import.meta.url)), "node_modules", "pagedjs", "dist", "paged.polyfill.min.js");
 
 async function applyPagedJs(page: Page): Promise<void> {
-  page.on("console", (message) => {
-    console.log("[BROWSER] %s", message.text());
-  });
   const propertyName = "__pagedjs_render_complete__" as const;
+  logEvents(page);
   await page.evaluate(
     ({ propertyName }) => {
       window[propertyName] = false;
       window.PagedConfig = {
-        auto: true,
         after() {
           console.debug("render complete");
           window[propertyName] = true;
         },
+        auto: true,
       };
     },
     { propertyName },
@@ -47,4 +45,27 @@ export default function pagedPdf(pages: PagesFunction | PagesMap): AstroIntegrat
     launch: { dumpio: true },
     pages,
   });
+}
+
+function logEvents(page: Page): void {
+  page.on("console", (message) => {
+    const type = message.type();
+    const text = message.text();
+    const args: string[] = text === "JSHandle@error"
+      ? message.args().map((arg) => arg.remoteObject().description ?? "<no description>")
+      : [text];
+    switch (type) {
+      case "error":
+      case "debug":
+      case "info":
+      case "trace":
+      case "warn":
+        console[type]("[BROWSER] %s", ...args);
+        break;
+      default:
+        console.log("[BROWSER] %s", ...args);
+    }
+  });
+  page.on("error", (err) => console.error("[BROWSER]", err));
+  page.on("pageerror", (err) => console.error("[BROWSER]", err));
 }
